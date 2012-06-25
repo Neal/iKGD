5c4a68037d1581fe16f0da564880d540c70671a2
Neal <neal@ineal.me>
2012-06-25 12:08:35 +0530

    fixed tabbing

--- FileIO.cs
+++ FileIO.cs
@@ -1,18 +1,18 @@
-﻿namespace iKGD
-{
-    using Microsoft.VisualBasic.CompilerServices;
-    using System;
-    using System.Collections;
-    using System.IO;
-    using System.Reflection;
-    using System.Runtime.InteropServices;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
-    internal sealed class FileIO
-    {		
+namespace iKGD
+{
+	internal sealed class FileIO
+	{		
 		public static bool Directory_Exists(string DirName)
 		{
-            if (string.IsNullOrEmpty(DirName))
-                return false;
+			if (string.IsNullOrEmpty(DirName))
+				return false;
 
 			return Directory.Exists(DirName);
 		}
@@ -36,41 +36,41 @@
 		public static bool Directory_Delete(string DirName)
 		{
 			if (!string.IsNullOrEmpty(DirName))
-            {
-                try
+			{
+				try
 				{
 					if (Directory_Exists(DirName))
 					{
 						Directory.Delete(DirName, true);
 						return true;
 					}
-                }
-                catch (Exception) { }
+				}
+				catch (Exception) { }
 			}
 			return false;
 		}
 
-        public static bool File_Delete(string FileName)
+		public static bool File_Delete(string FileName)
 		{
 			if (!string.IsNullOrEmpty(FileName))
-            {
-                try
-                {
+			{
+				try
+				{
 					File.Delete(FileName);
 					return true;
-                }
-                catch (Exception) { }
+				}
+				catch (Exception) { }
 			}
 			return false;
-        }
+		}
 
-        public static bool File_Exists(string FileName)
+		public static bool File_Exists(string FileName)
 		{
 			if (string.IsNullOrEmpty(FileName))
-                return false;
+				return false;
 
 			return File.Exists(FileName);
-        }
+		}
 
 		public static bool File_Copy(string SourceFile, string DestFile, bool overwrite)
 		{
@@ -86,68 +86,68 @@
 			return false;
 		}
 
-        public static bool File_Move(string SourceFile, string DestFile)
+		public static bool File_Move(string SourceFile, string DestFile)
 		{
 			if (!string.IsNullOrEmpty(SourceFile) && !string.IsNullOrEmpty(DestFile))
-            {
+			{
 				try
 				{
-                    File.Move(SourceFile, DestFile);
+					File.Move(SourceFile, DestFile);
 					return true;
 				}
 				catch (Exception) { }
 			}
 			return false;
-        }
+		}
 
-        public static bool File_Rename(string SourceFileName, string NewFileName)
-        {
+		public static bool File_Rename(string SourceFileName, string NewFileName)
+		{
 			if (!string.IsNullOrEmpty(SourceFileName) && !string.IsNullOrEmpty(NewFileName))
-            {
+			{
 				try
 				{
-                    File.Move(SourceFileName, NewFileName);
+					File.Move(SourceFileName, NewFileName);
 					return true;
 				}
 				catch (Exception) { }
 			}
 			return false;
-        }
+		}
 
-        public static string GetFileContents(string FilePath)
-        {
-            try
-            {
-                StreamReader reader = new StreamReader(FilePath);
-                string str = reader.ReadToEnd();
-                reader.Close();
-                return str;
-            }
-            catch (Exception) { }
-            return "";
-        }
+		public static string GetFileContents(string FilePath)
+		{
+			try
+			{
+				StreamReader reader = new StreamReader(FilePath);
+				string str = reader.ReadToEnd();
+				reader.Close();
+				return str;
+			}
+			catch (Exception) { }
+			return "";
+		}
 
-        public static long GetFileSize(string FilePath)
-        {
-            FileInfo info = new FileInfo(FilePath);
-            return info.Length;
-        }
+		public static long GetFileSize(string FilePath)
+		{
+			FileInfo info = new FileInfo(FilePath);
+			return info.Length;
+		}
 		
-        public static bool SaveTextToFile(string Data, string FilePath)
-        {
-            try
-            {
-                StreamWriter writer = new StreamWriter(FilePath);
-                writer.Write(Data);
-                writer.Close();
-                return true;
-            }
-            catch (Exception) { }
-            return false;
-        }
+		public static bool SaveTextToFile(string Data, string FilePath)
+		{
+			try
+			{
+				StreamWriter writer = new StreamWriter(FilePath);
+				writer.Write(Data);
+				writer.Close();
+				return true;
+			}
+			catch (Exception) { }
+			return false;
+		}
 
-        public static void SaveResourceToDisk(string resourceName, string fileName)
-        {
+		public static void SaveResourceToDisk(string resourceName, string fileName)
+		{
 			if (!File_Exists(fileName))
 			{
 				Assembly executingAssembly = Assembly.GetExecutingAssembly();
@@ -176,13 +176,13 @@
 					}
 				}
 			}
-        }
+		}
 
-        [DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern long GetWindowLong(IntPtr hWnd, int nIndex);
+		[DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern long GetWindowLong(IntPtr hWnd, int nIndex);
 
-        [DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern long SetWindowLong(IntPtr hWnd, int nIndex, long dwNewLong);
-    }
+		[DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern long SetWindowLong(IntPtr hWnd, int nIndex, long dwNewLong);
+	}
 }
 
